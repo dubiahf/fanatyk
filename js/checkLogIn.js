a1fd65eee62f237f0c87d0134fe0345a07e90f4e
dubiahf <76068508+dubiahf@users.noreply.github.com>
2020-12-20 04:49:01 +0100

    theme zrobiony
    ale są bugi w consoli i nie działa na account i login prawdopodobnie przez to

--- js/checkLogIn.js
+++ js/checkLogIn.js
@@ -13,5 +13,24 @@ window.onload=function(){
         document.getElementById('accLink').href="login.html"
       }
     };
+
+    // referencje z htmla do motywu
+
+    const body = document.body;
+    const theme = localStorage.getItem('theme');
+
+    if(theme){
+      body.classList.add(theme);
+    }
+
+    document.getElementById('dark').onclick=()=>{
+      body.classList.replace('light', 'dark');
+      localStorage.setItem('theme', 'dark');
+    };
+    
+    document.getElementById('light').onclick=()=>{
+      body.classList.replace('dark', 'light');
+      localStorage.setItem('theme', 'light');
+    };
 };
 
