26cd66ad64fbe2d1bedf99c12f2ef951e8035240
olkoskilol <76068511+olkoskilol@users.noreply.github.com>
2020-12-21 05:39:52 +0100

    Add files via upload

--- js/checkLogIn.js
+++ js/checkLogIn.js
@@ -1,4 +1,4 @@
-function checkLogIn(){
+window.onload=function(){
 
     // po kliknieciu na "konto" w navbarze przywołuje funkcję checkLogIn
     document.getElementById('accLink').onclick = function (){checkLogIn()};
@@ -12,6 +12,25 @@ function checkLogIn(){
       }else{
         document.getElementById('accLink').href="login.html"
       }
+    };
+
+    // referencje z htmla do motywu
+
+    const body = document.body;
+    const theme = localStorage.getItem('theme');
+
+    if(theme){
+      body.classList.add(theme);
+    }
+
+    document.getElementById('dark').onclick=()=>{
+      body.classList.replace('light', 'dark');
+      localStorage.setItem('theme', 'dark');
+    };
+    
+    document.getElementById('light').onclick=()=>{
+      body.classList.replace('dark', 'light');
+      localStorage.setItem('theme', 'light');
     };
 };
 
--- js/theme.js
+++ js/theme.js
@@ -1,35 +1,22 @@
-window.onload=function theme(){
-    // referencje z htmla do motywu
+// referencje z htmla do motywu
 
-    const body = document.body;
-    const theme = localStorage.getItem('theme');
+const body = document.body;
+const theme = localStorage.getItem('theme');
 
-    // jezeli w localStorage jest zchacheowany theme, zmien klase body
-    if(theme){
-        document.body.classList.add(theme);
-    }
+//jezeli w localStorage jest zchacheowany theme, zmien klase body
+// if(theme){
+//     document.body.classList.add(theme);
+// }
 
-    //przyciski podmieniaja klase body i zapisuja wybor do local storage
-    document.getElementById('dark').onclick=()=>{
-        body.classList.replace('light', 'dark');
-        localStorage.setItem('theme', 'dark');
-    };
-        
-    document.getElementById('light').onclick=()=>{
-        body.classList.replace('dark', 'light');
-        localStorage.setItem('theme', 'light');
-    };
+console.log(document.body.classList);
 
-    if( document.getElementById('accLink')){
-        // po kliknieciu na "konto" w navbarze przywołuje funkcję checkLogIn
-        document.getElementById('accLink').addEventListener("click", function (){
-            // funkcja sprawdza czy w session storage coś jest, jeżeli tak przekierowuje na account.html
-            // else href=login.html
-                if(sessionStorage.getItem('loggedInUn') != null){
-                document.getElementById('accLink').href="account.html"
-                }else{
-                document.getElementById('accLink').href="login.html"
-                }
-        });
-    };
+//przyciski podmieniaja klase body i zapisuja wybor do local storage
+document.getElementById('dark').onclick=()=>{
+    body.classList.replace('light', 'dark');
+    localStorage.setItem('theme', 'dark');
+};
+    
+document.getElementById('light').onclick=()=>{
+    body.classList.replace('dark', 'light');
+    localStorage.setItem('theme', 'light');
 };
